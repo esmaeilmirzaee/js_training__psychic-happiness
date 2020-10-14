041585177bd7915cb74b6dc45782e489729a1f83
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-14 05:23:47 -0400

    Fetch data

---
+++ promise_fetch_api.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+
+const randomUsers = (n) => {
+  const fetchUsers = fetch(`https://randomuser.me/api/?results=${n}`);
+  fetchUsers
+    .then((data) => {
+      data
+        .then((user) => {
+          console.log(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+randomUsers(10);
