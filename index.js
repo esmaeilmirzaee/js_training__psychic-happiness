147ed7e0ef7cfc923c87dd1a3893b323130f63c0
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-15 08:02:45 -0400

    NodeJS a sample web page

--- index.js
+++ index.js
@@ -4,40 +4,73 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, '/public/html', 'index.html'),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { ContentType: 'text/html' });
-        res.end(content);
-      }
-    );
+  let filePath = path.join(
+    __dirname,
+    'public/',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  // Extension of filePath
+  let extName = path.extname(filePath);
+
+  // Initial content type
+  let contentType = 'text/html';
+
+  switch (extName) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      filePath = path.join(
+        __dirname,
+        'public/',
+        req.url === '/' ? 'index.html' : req.url
+      );
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    default:
+      filePath = path.join(
+        __dirname,
+        'public/html/',
+        req.url === '/' ? 'index.html' : req.url
+      );
+      contentType = 'text/css';
+      break;
   }
 
-  // Show About page
-  if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public/html', 'about.html'),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { ContentType: 'text/html' });
-        res.end(content);
+  // Load file
+  fs.readFile(filePath, (err, content) => {
+    console.log(filePath);
+    if (err) {
+      if (err.code === 'ENONET') {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, '/public/html/', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { ContentType: 'text/html' });
+            res.end(content, 'utf8');
+          }
+        );
+      } else {
+        // Some server's errors
+        res.writeHead(500);
+        res.end(`Server's error ${err.code}`);
       }
-    );
-  }
-
-  if (req.url === '/api/user') {
-    const fetchUser = fetch('https://randomuser.me/api/?results=1');
-    res.writeHead(200, { ContentType: 'application/json' });
-    fetchUser
-      .then((data) => {
-        data.json().then((user) => {
-          res.end(JSON.stringify(user.results));
-        });
-      })
-      .catch((err) => console.log(err));
-  }
+    } else {
+      // Success
+      res.writeHead(200, { ContentType: 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
 const port = process.env.PORT || 5000;
